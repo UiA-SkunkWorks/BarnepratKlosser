3ebd0019cb0fbaab87cd4204c34f7a421975f834
madkle <Mads.madsen.klepper@gmail.com>
2024-04-12 14:23:59 +0200

    testspill
    tester ut barneprat

--- spill.mjs
+++ spill.mjs
@@ -21,14 +21,14 @@ const Start = (maal) => {
 }
 //#endregion
 
-Start(Velkommen);
+Start(Eggekurven);
 
 // Disse scenene er her som eksempler. Du kan ta dem bort eller endre dem. 
 
 function Velkommen() {
     const velkommen = new Blocks.Text("Trykk på denne teksten", { style: "overskrift", x: 0, y: 0, width: 100, height: 100 });
     Actions.Click(velkommen, () => {
-        GaaTil(scene2);
+        GaaTil(Eggekurven);
     })
 }
 
@@ -36,3 +36,15 @@ function scene2() {
     new Blocks.Text("Dette er en ny scene, som du ser pga at du trykket ", { style: "overskrift2", x: 0, y: 0, width: 500, height: 500 });
 }
 
+function Eggekurven()
+{
+
+// Å gjøre: Gi bedre navn til alle tingene.
+
+	const txt1 = new Blocks.Text("Fang eggene!", {style:"headingText", x:400,y:5,width:400,height:100});
+	const img2 = new Blocks.Image("resources/demo/egg.jpg", {x:200,y:10,width:100,height:100});
+	let test = Actions.Tween(img2,0,1)
+	Actions.Tween.Start(test);
+	const img1 = new Blocks.Image("resources/demo/basket.jpg", {x:200,y:550,width:100,height:100});
+	Actions.Drag(img1);
+}
