d7ed2aed86f7a4542982324803bd9e6960c2a813
Marnn9 <104505640+Marnn9@users.noreply.github.com>
2024-04-15 16:55:10 +0200

    added snap

--- barneprat/actions.mjs
+++ barneprat/actions.mjs
@@ -2,9 +2,9 @@ import CountDown from "./actions/countDown.mjs";
 import Click from "./actions/click.mjs";
 import { Drag, Drop } from "./actions/dragNdrop.mjs";
 import Tween from "./actions/tween.mjs";
-import Colide from "./actions/collision.mjs";
+import { Colide, Snap } from './actions/collision.mjs';
 
 
 
 
-export { CountDown, Click, Drag, Drop, Tween, Colide }
+export { CountDown, Click, Drag, Drop, Tween, Colide, Snap }
